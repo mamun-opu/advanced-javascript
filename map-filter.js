4f349cccd062a8bd51987a61a27055d89db5edcb
mamun1336 <abmamun504@gmail.com>
2020-04-19 02:48:56 +0600

    map-filter

---
+++ map-filter.js
@@ -0,0 +1,23 @@
+const numbers = [3, 4, 5, 6, 7, 8];
+// output = [];
+// for(let i = 0; i < numbers.length; i++){
+//     const element = numbers[i];
+//     const result = element*element;
+//     output.push(result);
+// }
+// function square(element) {
+//     return element*element;
+// }
+// const result = numbers.map(function square(element) {
+//     return element*element;
+// })
+
+// const result = numbers.map(x => x*x);
+
+// console.log(result);
+
+const bigger = numbers.filter(x => x >5);
+console.log(bigger);
+
+const isThere = numbers.find(x => x > 5);
+console.log((isThere));
