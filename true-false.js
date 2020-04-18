bb5e5a79f5adc4fc45009ed720fa8950a059ad93
mamun1336 <abmamun504@gmail.com>
2020-04-19 01:36:45 +0600

    true-false

---
+++ true-false.js
@@ -0,0 +1,31 @@
+//falsy...
+//false
+// 0
+// ""
+// undefined
+// null
+// NaN
+//truthy..
+//'0' 
+//' ' 
+//[]
+//true
+
+
+
+const age = [];
+if (age) {
+    console.log("condition is true");
+}
+else if(true){
+    console.log("condition2 is true");
+
+}
+else if(true){
+    console.log("condition3 is true");
+
+}
+else{
+    console.log("condition4 is true");
+
+}
