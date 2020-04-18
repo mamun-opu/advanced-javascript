d6b46deca4353055a9519d97ae77ea4c2c4d9fba
mamun1336 <abmamun504@gmail.com>
2020-04-19 02:27:01 +0600

    double vs triple vs equal

---
+++ double-vs-triple-vs-equal.js
@@ -0,0 +1,33 @@
+// const first = 2;
+// const second = "2";
+
+// if(first == second){
+//     console.log("== is true");
+// }
+// else{
+//     console.log("== is false");
+// }
+
+// if(first === second){
+//     console.log("=== is true");
+// }
+// else{
+//     console.log("=== is false");
+// }
+
+const first = 1;
+const second = true;
+
+if(first == second){
+    console.log("== is true");
+}
+else{
+    console.log("== is false");
+}
+
+if(first === second){
+    console.log("=== is true");
+}
+else{
+    console.log("=== is false");
+}
