04bf9eab37fe3a9318746677863a6123fbf9544f
mamun1336 <abmamun504@gmail.com>
2020-04-20 01:24:41 +0600

    break-continue

---
+++ break-continue.js
@@ -0,0 +1,11 @@
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+for(let i = 0; i < nums.length; i++){
+    if(nums[i] > 5)break;
+    console.log(nums[i]);
+}
+console.log("****************")
+for(let i = 0; i < nums.length; i++){
+    if(nums[i] % 2 == 1) continue;
+    console.log(nums[i]);
+}
