8e28dab068282f44e6c608bc730ef462d4194226
mamun1336 <abmamun504@gmail.com>
2020-04-20 00:25:44 +0600

    scope

---
+++ scope.js
@@ -0,0 +1,25 @@
+/* var diye kuno kichu declare korle shetake
+ tene uporer scope e niye jai. eke wasting bole!*/
+ /*but let ba const diye declare korle ta hoi na*/
+// console.log(day);
+let bonus = 20;
+function sum(first, second){
+    let result = first+second+bonus;
+    if(result > 9){
+        //var mood;
+        // const mood = "happy";
+       // console.log(mood);
+       var mood = "happy";
+    }
+    
+    console.log(mood);
+    console.log(day); 
+    // const day = "friday";
+    // let day = "friday";
+    var day = "friday";
+
+    console.log(day);
+    return result;
+}
+const output = sum(3, 7);
+console.log(output); 
